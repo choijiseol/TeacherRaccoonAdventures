c16526969e6ffaf82339e2d82a0bbfe3de57fe9d
최지설 <121646920+choijisul@users.noreply.github.com>
2024-06-03 20:09:17 +0900

    인트로 집 안 티비 키기 전 장면

--- js/introAnimation.js
+++ js/introAnimation.js
@@ -3,19 +3,39 @@ window.onload = function () {
     outsideHouse.classList.add('show'); // 첫 번째 애니메이션 적용
 };
 
-//3초 후 이미지 바뀜
-setTimeout(function() {
+// 텍스트 타이핑 효과
+function typeWriter(element, text, speed) {
+    let i = 0;
+    element.textContent = ''; // 기존 텍스트 초기화
+    function type() {
+        if (i < text.length) {
+            element.textContent += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+setTimeout(function () {
     var image = document.getElementById('house');
-    image.src = './img/introSceneImg/intro_insideHouse1.png'; 
-    image.style.width = '91%'; 
-    image.style.height = 'auto'; 
+    var dialogue = document.getElementById('dialogue');
+    image.src = './img/introSceneImg/intro_insideHouse1.png';
+    image.style.width = '91%';
+    image.style.height = 'auto';
+    typeWriter(dialogue, '주말인데 할 일이 없네ㅠㅠ. 티비나 봐야겠담!', 50);
+    dialogue.style.visibility = 'visible'; // 대사 보이게
 
-}, 4000); 
+    setTimeout(function () {
+        typeWriter(dialogue, '리모컨이... 여기있다!', 50);
+    }, 3000);
+}, 5000);
 
-setTimeout(function() {
+setTimeout(function () {
     var image = document.getElementById('house');
-    image.src = './img/introSceneImg/intro_insideHouse2.png'; 
-    image.style.width = '91%'; 
-    image.style.height = 'auto'; 
-
-}, 6000); 
+    var dialogue = document.getElementById('dialogue');
+    image.src = './img/introSceneImg/intro_insideHouse2.png';
+    image.style.width = '91%';
+    image.style.height = 'auto';
+    typeWriter(dialogue, '음...티비에서 뭐 하려나', 50);
+}, 10000);
