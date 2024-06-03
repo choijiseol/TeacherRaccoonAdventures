114668343ae10d99e2324fe6847b4ac169562b87
최지설 <121646920+choijisul@users.noreply.github.com>
2024-06-03 12:58:25 +0900

    인트로 집 안, 티비보는 모슨

--- js/introAnimation.js
+++ js/introAnimation.js
@@ -1,5 +1,13 @@
-window.onload = function() {
+window.onload = function () {
     var outsideHouse = document.querySelector('.outsideHouse');
     outsideHouse.classList.add('show'); // 첫 번째 애니메이션 적용
-
 };
+
+//3초 후 이미지 바뀜
+setTimeout(function() {
+    var image = document.getElementById('house');
+    image.src = './img/introSceneImg/intro_insideHouse1.png'; 
+    image.style.width = '91%'; 
+    image.style.height = 'auto'; 
+
+}, 4100); 
