b6f29696a00f4ebfdb68a40b3e71d5ca19428436
최지설 <121646920+choijisul@users.noreply.github.com>
2024-06-07 17:48:19 +0900

    인트로 티비화면 완료 (이미지, 대사, css 수정)

--- js/introAnimation.js
+++ js/introAnimation.js
@@ -1,21 +1,24 @@
 window.onload = function () {
     var outsideHouse = document.querySelector('.outsideHouse');
-    outsideHouse.classList.add('show'); // 첫 번째 애니메이션 적용
+    outsideHouse.classList.add('show'); //첫번째 보여줌
 };
 
-// 텍스트 타이핑 효과
-function typeWriter(element, text, speed) {
+// 텍스트 보드 이벤트
+function typeWriter(element, text, speed, callback) {
     let i = 0;
-    element.textContent = ''; // 기존 텍스트 초기화
+    element.textContent = ''; // 텍스트 초기화
     function type() {
         if (i < text.length) {
             element.textContent += text.charAt(i);
             i++;
             setTimeout(type, speed);
+        } else if (callback) {
+            callback();
         }
     }
     type();
 }
+
 
 setTimeout(function () {
     var image = document.getElementById('imageDiv');
@@ -48,3 +51,34 @@ setTimeout(function () {
     image.style.height = 'auto';
     typeWriter(dialogue, '오~ 홈쇼핑 원피스 이쁘다', 50);
 }, 12000);
+
+setTimeout(function () {
+    var image = document.getElementById('imageDiv');
+    var dialogue = document.getElementById('dialogue');
+    image.src = './img/introSceneImg/intro_viewTv2.png';
+    image.style.width = '91%';
+    image.style.height = 'auto';
+
+    // 텍스트 여러개인 경우
+    var texts = [
+        { text: '음?? 이게 뭐징', speed: 50 },
+        { text: '오! 루시 아일랜드...전설의 베이스!!!', speed: 100 },
+        { text: '갖고싶다... 한번 가볼까?', speed: 70 },
+        { text: '그래!!! 찾으러 여행을 떠나는거야!', speed: 70 },
+        { text: '먼저 필요한 짐부터 싸야겠당', speed: 100 }
+    ];
+
+    //하나씩 나타나게
+    function typeNextText(index) {
+        if (index < texts.length) {
+            typeWriter(dialogue, texts[index].text, texts[index].speed, function() {
+                setTimeout(function() {
+                    typeNextText(index + 1); 
+                }, 1000); // 테스트 간의 초 간격
+            });
+        }
+    }
+
+    // Start typing the first text
+    typeNextText(0);
+}, 14000);
